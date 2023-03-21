14a01091c9df5e81727fc8916c127af5d7f9a44c
Jesper Eriksson <84853548+jeppeerixon@users.noreply.github.com>
2023-03-21 10:50:55 +0100

    :zap: basic user requests done

--- backend/app.js
+++ backend/app.js
@@ -2,15 +2,24 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var productsRouter = require('./routes/products');
 var ordersRouter = require('./routes/orders');
 
-var app = express();
 
+var app = express();
 var base = '/api';
+
+//mongoDB connection
+const MongoClient = require('mongodb').MongoClient;
+MongoClient.connect('mongodb://127.0.0.1:27017', {
+    useUnifiedTopology: true
+}).then(client => {
+    console.log('MongoDB connected!')
+    const db = client.db('jesper-eriksson')
+    app.locals.db = db
+})
 
 app.use(logger('dev'));
 app.use(express.json());
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,8 +1,42 @@
 var express = require('express');
 var router = express.Router();
+const { ObjectId } = require("mongodb");
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  req.app.locals.db.collection('users').find().toArray()
+  .then(results => {
+    let printHTML = '<div><h3>Users:</h3>'
+
+    for (i in results) {
+      printHTML += '<p>' + results[i]._id + ' - ' + results[i].name + ' - ' + results[i].email + '</p>'
+    }
+
+    printHTML += '</div>'
+
+    res.send(printHTML);
+  })
+});
+
+router.post('/', function(req, res, next) {
+  const userId = req.body
+  console.log(userId)
+  req.app.locals.db.collection('users').findOne({"_id": new ObjectId(userId)})
+  .then(result => {
+    console.log("Hitta user", result);
+
+    res.json(result)
+  })
+});
+
+router.post('/add', function(req, res, next) {
+
+  req.app.locals.db.collection('users').insertOne(req.body)
+  .then(result => {
+    console.log("1 user inserted" + result);
+  })
+    
+
   res.send('USERS ROUTE');
 });
 
