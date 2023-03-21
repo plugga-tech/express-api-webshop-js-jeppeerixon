df7d514938d0587ecb92519ec4bff1820e5e4915
Jesper Eriksson <84853548+jeppeerixon@users.noreply.github.com>
2023-03-21 14:36:20 +0100

    :fire: added product routes

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,9 +1,50 @@
 var express = require('express');
 var router = express.Router();
+const { ObjectId } = require("mongodb");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('PRODUCTS');
+router.get('/', function(req, res, next) {  
+  req.app.locals.db.collection('products').find().toArray()
+  .then(results => {
+    let printHTML = '<div><h3>Products:</h3>'
+
+    for (i in results) {
+      printHTML += '<p>' + results[i]._id + ' - ' + results[i].name +
+      ' - ' + results[i].description + ' - ' + results[i].price + 
+      ' - ' + results[i].lager + 
+      '</p>'
+    }
+
+    printHTML += '</div>'
+
+    res.send(printHTML);
+  })
+});
+
+router.post('/add', function(req, res, next) {
+
+  req.app.locals.db.collection('products').insertOne(req.body)
+  .then(result => {
+    console.log("New product added" + result)
+  })
+    
+
+  res.send('Product added');
+});
+
+router.get('/:productId', function(req, res, next) {
+  productId = req.params.productId;
+  console.log(productId);
+
+  req.app.locals.db.collection("products").findOne({"_id": new ObjectId(productId)})
+  .then(result => {
+    if (result == null) {
+      res.status(500).json("Can't find products")
+    } else {
+      res.json(result)
+
+    }
+  })
 });
 
 module.exports = router;
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -20,12 +20,16 @@ router.get('/', function(req, res, next) {
 
 router.post('/', function(req, res, next) {
   const userId = req.body
+  //lägg till check för valid id key
   console.log(userId)
   req.app.locals.db.collection('users').findOne({"_id": new ObjectId(userId)})
   .then(result => {
-    console.log("Hitta user", result);
+    if (result == null) {
+      res.status(500).json("Can't find user")
+    } else {
+      res.json(result)
 
-    res.json(result)
+    }    
   })
 });
 
@@ -33,11 +37,29 @@ router.post('/add', function(req, res, next) {
 
   req.app.locals.db.collection('users').insertOne(req.body)
   .then(result => {
-    console.log("1 user inserted" + result);
+    console.log("1 user inserted" + result)
   })
     
 
   res.send('USERS ROUTE');
+});
+
+router.post('/login', function(req, res, next) {
+  const { email, password } = req.body;
+
+  req.app.locals.db.collection('users').findOne({"email": email})  
+  .then(result => {
+    if (result == null) {
+      res.status(500).json("Can't find user")
+    }
+    else if (result.password === password) {
+      console.log("Welcome: ")
+      res.json(result)
+    }
+    else {
+      res.status(401).json("Incorrect password or username")
+    }
+  })
 });
 
 module.exports = router;
